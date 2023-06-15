9b949f6bec1cae99b25f994222859393a77427b1
kawec91 <lukasz.lukawczyk@gmail.com>
2023-06-15 14:59:33 +0200

    Finish

--- 15.06.2023/index.js
+++ 15.06.2023/index.js
@@ -1,6 +1,15 @@
 //Verbindung
 const ver = document.getElementById("inhalt");
 
+//Array
+const myArray = [
+  { titel: "Titel 1", text: "fghjkhgfdehdgjjhgfdss" },
+  { titel: "Titel 2", text: "fghjkhgDSFGHfdes" },
+  { titel: "Titel 3", text: "fghjdfgdfghfkhgfdes" },
+  { titel: "Titel 4", text: "fghjkhgfdesjhhnbhbgfgdb" },
+  { titel: "Titel 5", text: "fghjkhgfdhgmjl.kjkles" },
+];
+
 //Bausteinfunktion
 function createText(text) {
   return `
@@ -10,10 +19,14 @@ function createText(text) {
 
 //Komponentfunction
 function createTitelAndText(titel, text) {
-  return `
+  ver.innerHTML += `
     <div>
         <h3>${titel}</h3>
         ${createText(text)}
     </div>
     `;
+}
+
+for (let x = 0; x < myArray.length; x++) {
+  createTitelAndText(myArray[x].titel, myArray[x].text);
 }
