fe700f723147e35188b1e9f30c977a0c8f561167
kawec91 <lukasz.lukawczyk@gmail.com>
2023-06-15 14:52:59 +0200

    CREATE: func createTitelAndText

--- 15.06.2023/index.js
+++ 15.06.2023/index.js
@@ -1,2 +1,19 @@
 //Verbindung
 const ver = document.getElementById("inhalt");
+
+//Bausteinfunktion
+function createText(text) {
+  return `
+    <p>${text}</p>
+    `;
+}
+
+//Komponentfunction
+function createTitelAndText(titel, text) {
+  return `
+    <div>
+        <h3>${titel}</h3>
+        ${createText(text)}
+    </div>
+    `;
+}
