d81aecd0613452b7f7617fa0baa09a0720bb9d92
kawec91 <lukasz.lukawczyk@gmail.com>
2023-06-16 10:12:29 +0200

    Loop, some style, some html channges

--- 15.06.2023/A5/index.js
+++ 15.06.2023/A5/index.js
@@ -6,7 +6,8 @@ const myArray = [
   {
     table_klasse: "table-class-1",
     row_klasse: "row-class-1",
-    product_image: "",
+    product_image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-13-pro-max-graphite-2023?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1679072989055",
     product_name: "Iphone",
     menge: "67",
     preis: "499.99",
@@ -14,7 +15,8 @@ const myArray = [
   {
     table_klasse: "table-class-2",
     row_klasse: "row-class-2",
-    product_image: "",
+    product_image:
+      "https://images.samsung.com/is/image/samsung/p6pim/de/2202/gallery/de-galaxy-s22-ultra-s908-sm-s908bdrdeub-thumb-530798314?imwidth=720",
     product_name: "Samsung",
     menge: "27",
     preis: "399.99",
@@ -22,7 +24,8 @@ const myArray = [
   {
     table_klasse: "table-class-3",
     row_klasse: "row-class-3",
-    product_image: "",
+    product_image:
+      "https://motorolade.vtexassets.com/arquivos/ids/158536-800-auto?width=800&height=auto&aspect=true",
     product_name: "Motorola",
     menge: "46",
     preis: "299.99",
@@ -31,10 +34,10 @@ const myArray = [
 
 //BauFunctionen
 function createImage(image, text) {
-  return `<img src="${image}" alt=""><br /><label>${text}</label>`;
+  return `<div class="img-wrapper"><img src="${image}" alt=""><br /><label>${text}</label><div>`;
 }
 function createMange(wert) {
-  return `<p>W${wert}x</p>`;
+  return `<p>${wert}x</p>`;
 }
 function createPreis(wert) {
   return `<p>${wert}€</p>`;
@@ -74,5 +77,12 @@ function createTable(
 }
 //Schleife
 for (let x = 0; x < myArray.length; x++) {
-  createTable();
+  createTable(
+    myArray[x].table_klasse,
+    myArray[x].row_klasse,
+    myArray[x].product_image,
+    myArray[x].product_name,
+    myArray[x].menge,
+    myArray[x].preis
+  );
 }
