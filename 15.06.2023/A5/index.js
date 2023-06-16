d6493dc7fbd92078cdcb4dce6d30987af61e8461
kawec91 <lukasz.lukawczyk@gmail.com>
2023-06-16 09:58:06 +0200

    function TableRow

--- 15.06.2023/A5/index.js
+++ 15.06.2023/A5/index.js
@@ -2,8 +2,8 @@
 const ver = document.getElementById("inhalt");
 
 //BauFunctionen
-function createImage(image) {
-  return `<img src="${image}" alt="">`;
+function createImage(image, text) {
+  return `<img src="${image}" alt=""><br /><label>${text}</label>`;
 }
 function createMange(wert) {
   return `<p>W${wert}x</p>`;
@@ -11,3 +11,14 @@ function createMange(wert) {
 function createPreis(wert) {
   return `<p>${wert}€</p>`;
 }
+
+function createTableRow(klasse, image, productName, mengeWert, preisWert) {
+  return `
+    <tr class="${klasse}">
+        <td>${createImage(image, productName)}</td>
+        <td>${createMange(mengeWert)}</td>
+        <td>${createPreis(preisWert)}</td>
+    </tr>
+    `;
+}
+//Komponentfunctionen
